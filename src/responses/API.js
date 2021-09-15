b111141aedf06226518e2cbf3df1ea1abd01fcda
steelswheel <steelswheel@gmail.com>
2021-09-15 14:02:37 +0500

    v.1.0.1

---
+++ src/responses/API.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+
+export const API = {
+    async fetchPosts({state, commit}) {
+        try {
+            commit('setLoading', true);
+            const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
+                params: {
+                    _page: state.page,
+                    _limit: state.limit
+                }
+            });
+            commit('setTotalPages', Math.ceil(response.headers['x-total-count'] / state.limit));
+            commit('setPosts', response.data);
+        } catch (e) {
+            console.log(e);
+        } finally {
+            commit('setLoading', false);
+        }
+    },
+    async loadMorePosts({state, commit}) {
+        try {
+            commit('setPage', state.page += 1);
+            const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
+                params: {
+                    _page: state.page,
+                    _limit: state.limit
+                }
+            });
+            commit('setTotalPages', Math.ceil(response.headers['x-total-count'] / state.limit));
+            commit('setPosts', [...state.posts, ...response.data]);
+        } catch (e) {
+            alert('Ошибка: ' + e);
+        }
+    }
+}
--- src/store/postModule.js
+++ src/store/postModule.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import {API} from '@/responses/API';
 
 export const postModule = {
     state: () => ({//здесь данные, которые будут использоваться в приложении
@@ -44,38 +44,8 @@ export const postModule = {
         }
     },
     actions: {//функции, использующие внутри себя мутации, получаем данные с сервера и сохраняем данные в state
-        async fetchPosts({state, commit}) {
-            try {
-                commit('setLoading', true);
-                const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
-                    params: {
-                        _page: state.page,
-                        _limit: state.limit
-                    }
-                });
-                commit('setTotalPages', Math.ceil(response.headers['x-total-count'] / state.limit));
-                commit('setPosts', response.data);
-            } catch (e) {
-                console.log(e);
-            } finally {
-                commit('setLoading', false);
-            }
-        },
-        async loadMorePosts({state, commit}) {
-            try {
-                commit('setPage', state.page += 1);
-                const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
-                    params: {
-                        _page: state.page,
-                        _limit: state.limit
-                    }
-                });
-                commit('setTotalPages', Math.ceil(response.headers['x-total-count'] / state.limit));
-                commit('setPosts', [...state.posts, ...response.data]);
-            } catch (e) {
-                alert('Ошибка: ' + e);
-            }
-        }
+        fetchPosts: API.fetchPosts,
+        loadMorePosts: API.loadMorePosts,
     },
     namespaced: true
 }
